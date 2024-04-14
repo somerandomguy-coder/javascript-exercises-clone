b032771e2fe11dd067366dd9e6183344e53e2952
somerandomguy-coder <153010587+somerandomguy-coder@users.noreply.github.com>
2024-04-15 05:35:46 +1000

    add solution part 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    cleanedWordList = clean(word).split("");
+    let index = 0
+    for (let i = cleanedWordList.length-1; i>-1; i--){
+        if (cleanedWordList[index] != cleanedWordList[i]){
+            return false;
+        }
+        index += 1
+    }
+    return true;
 };
+
+function clean(word){
+    return word.split(",").join("").split(" ").join("").split(".").join("").split("!").join("").toLowerCase();
+}
+
 
 // Do not edit below this line
 module.exports = palindromes;
