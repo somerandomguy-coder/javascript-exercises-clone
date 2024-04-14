38f8885238944b0495e2f22d74881b5c25392df4
somerandomguy-coder <153010587+somerandomguy-coder@users.noreply.github.com>
2024-04-15 05:23:06 +1000

    add solution part 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(list) {
+  let result = 0;
+  for (let i=0; i<list.length; i++){
+    result += list[i];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(list) {
+  let result = 1;
+  for (let i=0; i<list.length; i++){
+    result *= list[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num == 1 || num == 0){
+    return 1;
+  } else {
+    return num * factorial(num-1);
+  }
 };
 
 // Do not edit below this line
