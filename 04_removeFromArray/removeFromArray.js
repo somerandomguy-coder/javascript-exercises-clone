a113935e5f2a20ba52b26cc8950b2138d4dd6c1e
somerandomguy-coder <153010587+somerandomguy-coder@users.noreply.github.com>
2024-04-14 15:39:53 +1000

    add solution part 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(list, ...removelist) {
+    let array = [];
+    for (let i=0; i<list.length; i++){
+        let match = false;
+        for (let j=0; j<removelist.length; j++){
+            if (list[i] === removelist[j]){
+                match = true;
+            }
+        }
+        if (match == false){
+            array.push(list[i]);
+        }
+    }
+    return array;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
