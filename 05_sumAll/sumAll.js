f1a5f5b6fea12a996c9c1af7d06f0865c45ec4ae
somerandomguy-coder <153010587+somerandomguy-coder@users.noreply.github.com>
2024-04-15 04:53:43 +1000

    add solution part 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(first, second) {
+    if (first < 0 || second < 0 || typeof(first) != "number" || typeof(second) != "number"){
+        return "ERROR";
+    } else if (first < second) {
+        return sum(first, second);
+    } else if (first > second) {
+        return sum(second, first);
+    }
 };
+
+function sum(start, end){
+    let result = 0;
+    for (let i=start; i<=end; i++){
+        result += i;
+    }
+    return result;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
