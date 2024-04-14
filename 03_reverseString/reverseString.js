3244b0c581194fa47acba00d2298a21f4dde2c24
somerandomguy-coder <153010587+somerandomguy-coder@users.noreply.github.com>
2024-04-14 15:11:29 +1000

    add solution for exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let result = "";
+    for (let i=0; i<string.length; i++){
+        result = string[i] + result;
+    }
+    return result;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
