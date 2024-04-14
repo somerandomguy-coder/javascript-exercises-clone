b9b8d474a706c6e812be61adc37d0a016141b37e
somerandomguy-coder <153010587+somerandomguy-coder@users.noreply.github.com>
2024-04-15 06:06:00 +1000

    add solution part 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,17 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(list) {
+    const titles = [];
+    list.forEach(element => {
+        keys = Object.keys(element);
+        values = Object.values(element);
+        for (let i=0; i<keys.length; i++){
+            if (keys[i] == "title"){
+                titles.push(values[i]);
+            }
+        }
+    });
+    return titles;
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
